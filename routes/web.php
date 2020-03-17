d86f2efe203916b95948ca7f42be9b5e928468e2
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-17 16:57:34 +0200

    created app.blade.php as index view for spa

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::any('{all}', function () {
+    return view('app');
+})->where('all', '^(?!api).*$');
