67c00e7360f945adf856df94ccb026ae1d822697
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-18 17:05:29 +0200

    added helpers

--- resources/js/helpers/index.js
+++ resources/js/helpers/index.js
@@ -8,4 +8,42 @@ const isLoggedIn = () => {
     return !!JSON.parse(window.localStorage.getItem('authToken'));
 };
 
-export { authHeader, isLoggedIn };
+const isObject = val => {
+    return (!!val) && (val.constructor === Object);
+};
+
+const isArray = val => {
+    return (!!val) && (val.constructor === Array);
+};
+
+const hasError = (errors = {}) => {
+    for (let key in errors) {
+        if (errors[key]) {
+
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const processServerErrors = errors => {
+    let result = [];
+    if (isObject(errors)) {
+        for (let key in errors) {
+            if (isArray(errors[key])) {
+                errors[key].forEach(err => {
+                    result.push(err);
+                })
+            } else {
+                result.push(errors[key].toString())
+            }
+        }
+    } else {
+        result = isArray(errors) ? errors : errors.toString();
+    }
+
+    return result;
+};
+
+export { authHeader, isLoggedIn, isObject, isArray, hasError, processServerErrors };
