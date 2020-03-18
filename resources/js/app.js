61728233c6528c6a33516f9d5b97316c10437b8a
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-18 09:31:12 +0200

    created store for app

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,26 +7,36 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+import reducer from './reducers';
 
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/login">
-                    <LoginPage />
-                </Route>
-                <Route path="/register">
-                    <RegisterPage />
-                </Route>
-                <Route path="/">
-                    <MainPage />
-                </Route>
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route path="/register">
+                        <RegisterPage />
+                    </Route>
+                    <Route path="/">
+                        <MainPage />
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
     );
 }
 
