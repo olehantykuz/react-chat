4e7df6abdfc32c4bae4fdf4a56845021a9bf62dd
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-18 09:30:22 +0200

    fixed title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Super Shop</title>
+        <title>Online Chat</title>
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
