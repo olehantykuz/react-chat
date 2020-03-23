1fd9cc815ab42714d640de11e9b272ccc68a808e
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-23 17:06:35 +0200

    created event MessageSent

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Message details
+     *
+     * @var Message
+     */
+    public $message;
+
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Message $message
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat');
+    }
+
+    /**
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'message.send';
+    }
+}
