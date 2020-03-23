d2c078e7e5bbc046c25851ba89103c6b4a6995b0
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-23 09:03:46 +0200

    created action and reducer chat

--- resources/js/reducers/index.js
+++ resources/js/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import user from './user';
 import auth from './auth';
+import chat from './chat';
 
 export default combineReducers({
     user,
     auth,
+    chat,
 });
