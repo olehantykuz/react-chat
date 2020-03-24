6a265fe575d2e5bb86949c2bb1758602bfb798eb
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-24 17:25:19 +0200

    created service, reducer and actions config

---
+++ resources/js/reducers/config.js
@@ -0,0 +1,14 @@
+const config = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_CONFIG': {
+            return action.data
+        }
+        case 'CLEAR_CONFIG': {
+            return {}
+        }
+        default:
+            return state;
+    }
+};
+
+export default config;
