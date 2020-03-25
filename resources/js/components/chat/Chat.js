47ab70a7865ad56baf46cae3fd9d0527bbe4ed7c
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-25 16:48:37 +0200

    updated styles of chat element

--- resources/js/components/chat/Chat.js
+++ resources/js/components/chat/Chat.js
@@ -6,14 +6,35 @@ function Chat(props) {
     return (
         <div className="row justify-content-center">
             <div className="col-md-8">
-                <div className="panel panel-default">
-                    <div className="panel-heading">Chats</div>
+                <div className="card w-100">
+                    <div className="card-body">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">Chats</div>
 
-                    <div className="panel-body">
-                        <ChatMessages />
+                            <div className="panel-body">
+                                <ChatMessages />
+                            </div>
+                            <div className="panel-footer">
+                                <ChatMessagesForm />
+                            </div>
+                        </div>
                     </div>
-                    <div className="panel-footer">
-                        <ChatMessagesForm />
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card w-100">
+                    <div className="card-body">
+                        <h5>Search</h5>
+                    </div>
+                </div>
+                <div className="card w-100">
+                    <div className="card-body">
+                        <h5>Friends</h5>
+                    </div>
+                </div>
+                <div className="card w-100">
+                    <div className="card-body">
+                        <h5>Groups</h5>
                     </div>
                 </div>
             </div>
