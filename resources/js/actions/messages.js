14cbfd00a2c0560f3acc356ee50e0ab2240382c6
Oleh Antykuz <ovantykuz@gmail.com>
2020-03-26 17:17:54 +0200

    created actionTypes, action and reducer messages

---
+++ resources/js/actions/messages.js
@@ -0,0 +1,21 @@
+import { SET_MESSAGE, REMOVE_MESSAGE, CLEAR_MESSAGES, ADD_MESSAGES } from '../actionTypes/messages';
+
+export const setMessage = (id, data) => ({
+    type: SET_MESSAGE,
+    id,
+    data
+});
+
+export const addMessages = items => ({
+    type: ADD_MESSAGES,
+    items
+});
+
+export const removeMessage = id => ({
+    type: REMOVE_MESSAGE,
+    id
+});
+
+export const clearMessage = () => ({
+    type: CLEAR_MESSAGES
+});
