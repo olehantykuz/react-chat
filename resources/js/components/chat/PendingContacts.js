3e107b1847f4653c7e29d9dbe1b7b88aab962d89
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-02 13:35:12 +0300

    processed confirm friendship click

--- resources/js/components/chat/PendingContacts.js
+++ resources/js/components/chat/PendingContacts.js
@@ -1,8 +1,17 @@
 import React, {Fragment} from 'react';
 import {connect} from 'react-redux';
 
+import {confirmFriendRequest} from '../../actions/contacts';
+import {clearServerErrors} from '../../actions/errors';
+import ServerErrors from '../auth/fields/ServerErrors';
+
 function PendingContacts(props) {
     const { pending, requests } = props.contacts;
+
+    const handleClick = (id) => {
+        props.clearServerErrors();
+        props.confirmFriendRequest(id);
+    };
 
     return (
         <Fragment>
@@ -24,13 +33,20 @@ function PendingContacts(props) {
             {requests.length > 0 &&
             <div className="invitation_contacts">
                 <h6 className="text-center">Invitation</h6>
+                <ServerErrors
+                    serverErrors={props.errors.confirmContact}
+                />
                 {props.contacts.requests.map((id, index) => {
                     const user = props.users[id];
 
                     return user !== undefined ? (
                         <div key={id} className="invitation_contacts--item">
                             <span>{index + 1}. {props.users[id].name}</span>
-                            <button className="btn btn-success">
+                            <button
+                                className="btn btn-success"
+                                onClick={() => {handleClick(id)}}
+                                disabled={props.contacts.isConfirmingNewFriend}
+                            >
                                 Add
                             </button>
                         </div>
@@ -44,8 +60,12 @@ function PendingContacts(props) {
 
 const mapStateToProps = state => ({
     users: state.users,
-    contacts: state.contacts
+    contacts: state.contacts,
+    errors: state.errors,
 });
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+    confirmFriendRequest: id => dispatch(confirmFriendRequest(id)),
+    clearServerErrors: () => dispatch(clearServerErrors()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PendingContacts);
