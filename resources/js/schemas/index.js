1005165a2691c9bbb7a57def7afa1f44d30f08e1
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-02 11:30:00 +0300

    added userSchemaWithContacts

--- resources/js/schemas/index.js
+++ resources/js/schemas/index.js
@@ -1,7 +1,15 @@
 import { schema } from 'normalizr';
 
 export const userSchema = new schema.Entity('users');
+export const userSchemaWithContacts = new schema.Entity('users');
 export const messageSchema = new schema.Entity('messages');
+
+userSchemaWithContacts.define({
+    requestedFriendsTo: [userSchema],
+    requestFriendsBy: [userSchema],
+    friends: [userSchema],
+});
+
 messageSchema.define({
     user: userSchema
 });
