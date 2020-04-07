2c35d0b3fb40eebe4ab27ce283ac5d4173ce66ef
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-07 11:11:07 +0300

    deleted chat reducer, actions and action types

--- resources/js/reducers/index.js
+++ resources/js/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 
 import errors from './errors';
 import auth from './auth';
-import chat from './chat';
 import config from './config';
 import users from './users';
 import messages from './messages';
@@ -14,7 +13,6 @@ export default combineReducers({
     config,
     users,
     auth,
-    chat,
     messages,
     contacts,
     rooms,
