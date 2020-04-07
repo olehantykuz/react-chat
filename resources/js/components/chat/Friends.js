199bd060d1fa53df2c1fd411d76ab1825178ba86
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-07 09:40:55 +0300

    updated Friends component

--- resources/js/components/chat/Friends.js
+++ resources/js/components/chat/Friends.js
@@ -1,7 +1,25 @@
 import React, {Fragment} from 'react';
 import {connect} from 'react-redux';
+import {toggleChat} from '../../actions/chats';
 
 function Friends(props) {
+    const handleClick = id => {
+        if (id) {
+            props.toggleChat(id);
+        }
+    };
+
+    const getRoomIdByFriendId = friendId => {
+        let result = null;
+        Object.values(props.rooms).forEach(room => {
+            if (room.users.indexOf(friendId) > -1) {
+                result = room.id;
+            }
+        });
+
+        return result;
+    };
+
     return (
         <Fragment>
             <h5 className="text-center">Friends</h5>
@@ -9,9 +27,14 @@ function Friends(props) {
             <div className="friends_contacts">
                 {props.contacts.friends.map((id, index) => {
                     const user = props.users[id];
+                    const roomId = getRoomIdByFriendId(id);
 
                     return user !== undefined ? (
-                        <div key={id} className="friends_contacts--item">
+                        <div
+                            key={id}
+                            className="friends_contacts--item"
+                            onClick={() => handleClick(roomId)}
+                        >
                             <span>{index + 1}. {props.users[id].name}</span>
                         </div>
                     ) : null;
@@ -25,6 +48,10 @@ function Friends(props) {
 const mapStateToProps = state => ({
     contacts: state.contacts,
     users: state.users,
+    rooms: state.rooms,
+});
+const mapDispatchToProps = dispatch => ({
+    toggleChat: id => dispatch(toggleChat(id)),
 });
 
-export default connect(mapStateToProps)(Friends);
+export default connect(mapStateToProps, mapDispatchToProps)(Friends);
