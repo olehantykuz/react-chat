bb5598017a82bebed2d21d28ada9690ac24c92bf
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-07 09:49:32 +0300

    updated Chat component

--- resources/js/components/chat/Chat.js
+++ resources/js/components/chat/Chat.js
@@ -6,32 +6,29 @@ import ChatMessagesForm from './ChatMessagesForm';
 import SearchNewContacts from './SearchNewContacts';
 import PendingContacts from './PendingContacts';
 import Friends from './Friends';
-import { isLoggedIn } from '../../helpers';
-import { addNewChatMessage, fetchMessages } from '../../actions/chat';
 import { addFriendRequest, addToFriendsFromRequested } from '../../actions/contacts';
+import { fetchChatMessages, addMessageToRoom } from '../../actions/chats';
 
 function Chat(props) {
+    useEffect(() => {
+        if (props.chats.active) {
+            props.fetchChatMessages(props.chats.active);
+        }
+    }, [props.chats.active]);
+
     useEffect(() => {
         const { broadcastChannelPrefix } = props.config;
         const hasConfigs = broadcastChannelPrefix !== undefined;
         let chatChannelName = '';
         let addFriendChannelName = '';
         let confirmedFriendChannelName = '';
+        const roomsChannelsNames = [];
 
         if (hasConfigs && props.auth.id) {
             chatChannelName = broadcastChannelPrefix + 'private-chat';
             addFriendChannelName = broadcastChannelPrefix + 'private-request.friend.to.' + props.auth.id;
             confirmedFriendChannelName =broadcastChannelPrefix + 'private-confirm.friend.to.' + props.auth.id;
 
-            Echo.channel(chatChannelName)
-                .listen('.message.send', e => {
-                    const { message } = e;
-
-                    //TODO: Fix self listening for redis driver
-                    if (message.user.id !== props.auth.id) {
-                        props.addNewChatMessage(message);
-                    }
-                });
             Echo.channel(addFriendChannelName)
                 .listen('.request.friend', e => {
                     props.addFriendRequest(e.sender);
@@ -40,18 +37,34 @@ function Chat(props) {
                 .listen('.confirm.friend', e => {
                     props.addToFriendsFromRequested(e.sender);
                 });
-        }
 
-        if (isLoggedIn() && props.chat.messages.length === 0 && !props.chat.fetchingMessage) {
-            props.fetchMessages();
+            Object.keys(props.rooms).forEach(room => {
+                const cnName = broadcastChannelPrefix + 'private-message.to.room.' + room;
+                roomsChannelsNames.push(cnName);
+
+                Echo.channel(cnName)
+                    .listen('.message.send', e => {
+                        const { message, room} = e;
+                        console.log({e, message, room, active: props.chats.active});
+                        props.addMessageToRoom(message, room.id);
+                    });
+            });
         }
 
         return () => {
             chatChannelName && Echo.leave(chatChannelName);
             addFriendChannelName && Echo.leave(addFriendChannelName);
+            roomsChannelsNames.forEach(name => {
+                Echo.leave(name);
+            });
         }
 
-    }, [props.config, props.auth.id]);
+    }, [props.config, props.auth.id, props.rooms]);
+
+    const selectedRoomId = props.chats.active;
+    const chatName = selectedRoomId
+        ? (props.rooms[selectedRoomId].name || 'Room ' + selectedRoomId)
+        : 'No selected Chat';
 
     return (
         <div className="row justify-content-center">
@@ -71,7 +84,7 @@ function Chat(props) {
                 <div className="card w-100">
                     <div className="card-body">
                         <div className="panel panel-default">
-                            <div className="panel-heading">Chats</div>
+                            <div className="panel-heading">{chatName}</div>
 
                             <div className="panel-body">
                                 <ChatMessages />
@@ -100,15 +113,16 @@ function Chat(props) {
 }
 
 const mapStateToProps = state => ({
-    chat: state.chat,
     users: state.users,
     messages: state.messages,
     auth: state.auth,
     config: state.config,
+    rooms: state.rooms,
+    chats: state.chats,
 });
 const mapDispatchToProps = dispatch => ({
-    fetchMessages: () => dispatch(fetchMessages()),
-    addNewChatMessage: payload => dispatch(addNewChatMessage(payload)),
+    fetchChatMessages: roomId => dispatch(fetchChatMessages(roomId)),
+    addMessageToRoom: (payload, roomId) => dispatch(addMessageToRoom(payload, roomId)),
     addFriendRequest: payload => dispatch(addFriendRequest(payload)),
     addToFriendsFromRequested: payload => dispatch(addToFriendsFromRequested(payload)),
 });
