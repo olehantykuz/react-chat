0c70d2bb553e4eb5da59d6105651690fd47ee226
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-07 09:28:01 +0300

    created rooms and chats action types

---
+++ resources/js/actionTypes/rooms.js
@@ -0,0 +1,5 @@
+export const ADD_ROOM = 'ADD_ROOM';
+export const ADD_ROOMS = 'ADD_ROOMS';
+export const SET_ROOMS = 'SET_ROOMS';
+export const REMOVE_ROOM = 'REMOVE_ROOM';
+export const CLEAR_ROOMS = 'CLEAR_ROOMS';
