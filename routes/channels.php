77054435e2330a135691316509741eabc9f352d4
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-10 11:03:14 +0300

    updated auth logic for channels

--- routes/channels.php
+++ routes/channels.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('request.friend.to.{userId}', function ($user, $userId) {
-    return \Auth::check();
+    return (int) $user->id === (int) $userId;
 });
 
 Broadcast::channel('confirm.friend.to.{userId}', function ($user, $userId) {
-    return \Auth::check();
+    return (int) $user->id === (int) $userId;
 });
 
 Broadcast::channel('message.to.room.{userId}', function ($user, $roomId) {
