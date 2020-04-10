c07492555aba91c20654176658c6e5e65d9a33c1
Oleh Antykuz <ovantykuz@gmail.com>
2020-04-10 11:06:29 +0300

    set to localstorage broatcasting configs

--- resources/js/services/config.js
+++ resources/js/services/config.js
@@ -4,7 +4,14 @@ const fetchConfigs = () => {
     return axios({
         url: `/api/config`,
         method: 'get',
-    }).then(response => response);
+    }).then(response => {
+        if (response.status < 400) {
+            localStorage.setItem('broadcastChannelPrefix', JSON.stringify(response.data.config.broadcastChannelPrefix));
+            localStorage.setItem('broadcastDriver', JSON.stringify(response.data.config.broadcastDriver));
+        }
+
+        return response
+    });
 };
 
 export const configService = {
